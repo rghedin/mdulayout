c74651aec80bbeed42328af8f530ee0e9629e713
Rodrigo Ghedin <ghedin@gmail.com>
2017-10-16 18:20:12 -0200

    Adequações para Gazeta do Povo

--- footer.php
+++ footer.php
@@ -12,14 +12,6 @@
 
 	</div><!-- .site-content -->
 
-<<<<<<< HEAD
-=======
-	<div class="compre">
-		<p>Compre pelos links abaixo. O valor não muda e você ajuda o site:</p>
-		<a href="https://www.amazon.com.br//ref=as_li_ss_tl?ie=UTF8&linkCode=ll2&tag=manudousua-20&linkId=39089c290b633512dc478545e818dd5e" target="_blank">Amazon</a> &bull; <a href="http://www.americanas.com.br/?franq=AFL-03-37228&loja=02" target="_blank">Americanas</a> &bull; <a href="https://ad.zanox.com/ppc/?36601608C1370714384&ULP=[[https://www.cissamagazine.com.br/?utm_medium=afiliados&utm_source=zanox&utm_campaign=deeplink_generator&utm_term=zanox&channel=29646554]]" target="_blank">Cissa Magazine</a> &bull; <a href="https://ad.zanox.com/ppc/?40191652C86843001&ULP=[[http://www.pontofrio.com.br/?utm_source=zanox&utm_medium=deeplink&utm_campaign=deeplink]]" target="_blank">Ponto Frio</a> &bull; <a href="https://ad.zanox.com/ppc/?40349327C16971995&ULP=[[http://www.ricardoeletro.com.br/?utm_source=zanox&utm_medium=deeplink_RE&utm_campaign=userid&prc=8803&utm_content=DeeplinkGenerator]]">Ricardo Eletro</a> &bull; <a href="https://ad.zanox.com/ppc/?40266771C43506995&ULP=[[http://www.saraiva.com.br/?PAC_ID=125109&utm_source=zanox&utm_medium=afiliados&utm_campaign=deeplink]]" target="_blank">Saraiva</a> &bull; <a href="http://www.shoptime.com.br/?franq=AFL-03-37228&loja=01" target="_blank">Shoptime</a> &bull; <a href="http://www.submarino.com.br/?franq=AFL-03-37228&loja=03" target="_blank">Submarino</a> &bull; <a href="https://ad.zanox.com/ppc/?40255055C93242807&ULP=[[https://www.walmart.com.br/?utm_source=zanox&utm_medium=afiliados&utm_campaign=custom_deeplink]]" target="_blank">Walmart</a>
-	</div>
-
->>>>>>> 0246b4e3fd7045b21098da78d810dc53998aa98a
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php
